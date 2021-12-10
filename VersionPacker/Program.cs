fdd81468a7f8eb41cb437cd9578ef12b0aaf1f93
Pyrdacor <trobt@web.de>
2021-12-10 17:33:51 +0100

    Added feature support

--- VersionPacker/Program.cs
+++ VersionPacker/Program.cs
@@ -34,11 +34,13 @@ namespace VersionPacker
                 var language = parts[1];
                 var info = parts[2];
                 var file = parts[3];
+                var features = int.Parse(parts[4]);
                 uint offset = (uint)writer.BaseStream.Position;
 
                 writer.Write(version);
                 writer.Write(language);
                 writer.Write(info);
+                writer.Write((byte)features);
                 if (!Path.IsPathRooted(file))
                     file = Path.Combine(Path.GetDirectoryName(args[0]), file);
                 var bytes = File.ReadAllBytes(file);

1a54c7811cfee6c09c17bf1e1037883c0b65190a
Pyrdacor <trobt@web.de>
2024-02-19 16:44:30 +0100

    Added intro and extro text packers

---
+++ AmbermoonExtroTextPacker/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Text;
+using Ambermoon.Data.Legacy.Serialization;
+
+namespace AmbermoonExtroTextPacker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var outroTexts = new List<List<string>>[6] { new(), new(), new(), new(), new(), new() };
+            int clickGroupIndex = 0;
+
+            var path = @"D:\Projekte\Ambermoon\Disks\Bugfixing\Polish\ExtroTextGroups";
+
+            foreach (var clickGroup in Directory.GetDirectories(path).OrderBy(d => int.Parse(Path.GetFileName(d)[0..3])))
+            {
+                var clickGroupTexts = outroTexts[clickGroupIndex++];
+
+                foreach (var group in Directory.GetDirectories(clickGroup).OrderBy(d => int.Parse(Path.GetFileName(d)[0..3])))
+                {
+                    var groupTexts = new List<string>();
+
+                    foreach (var file in Directory.GetFiles(group).OrderBy(f => int.Parse(Path.GetFileName(f)[0..3])))
+                    {
+                        string text = File.ReadAllText(file, Encoding.UTF8);
+                        groupTexts.Add(text);
+                    }
+
+                    clickGroupTexts.Add(groupTexts);
+                }
+            }
+            
+
+            var dataWriter = new DataWriter();
+
+            dataWriter.Write((ushort)6);
+
+            for (int i = 0; i < 6; ++i)
+                dataWriter.Write((ushort)outroTexts[i].Count);
+
+            foreach (var clickGroup in outroTexts)
+            {
+                for (int i = 0; i < clickGroup.Count; ++i)
+                    dataWriter.Write((ushort)clickGroup[i].Count);
+
+                foreach (var group in clickGroup)
+                {
+                    foreach (var text in group)
+                    {
+                        dataWriter.WriteNullTerminated(text, Encoding.UTF8);
+                    }
+                }
+
+                if (dataWriter.Size % 2 == 1)
+                    dataWriter.Write((byte)0);
+            }
+
+            dataWriter.Write((ushort)1); // Number of translators
+            dataWriter.WriteNullTerminated("galon3"); // Translator (note: ensure UTF8)
+
+            dataWriter.WriteNullTerminated("<KLIKNIJ>", Encoding.UTF8);
+
+            if (dataWriter.Size % 2 == 1)
+                dataWriter.Write((byte)0);
+
+            File.WriteAllBytes(@"D:\Projekte\Ambermoon\Disks\Bugfixing\Polish\Extro_texts.amb", dataWriter.ToArray());
+        }
+    }
+}
---
+++ AmbermoonIntroTextPacker/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Text;
+using Ambermoon.Data.Legacy.Serialization;
+using TextWriter = Ambermoon.Data.Legacy.Serialization.TextWriter;
+
+namespace AmbermoonIntroTextPacker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var nonCommandTexts = new List<string>();
+            var commandTexts = new List<string[]>();
+            var currentCommandTexts = new List<string>();
+            var path = @"D:\Projekte\Ambermoon\Disks\Bugfixing\Polish\IntroTexts";
+
+            foreach (var file in Directory.GetFiles(path).OrderBy(f => int.Parse(Path.GetFileName(f)[0..3])).ThenBy(f =>
+            {
+                var fname = Path.GetFileNameWithoutExtension(f);
+                if (fname.Contains('.'))
+                    return int.Parse(fname[4..7]);
+                return 0;
+            }))
+            {
+                var fname = Path.GetFileNameWithoutExtension(file);
+                string text = File.ReadAllText(file, Encoding.UTF8);
+
+                if (fname.Contains('.'))
+                {
+                    if (int.Parse(fname[4..7]) == 0) // New command
+                    {
+                        if (currentCommandTexts.Count != 0)
+                        {
+                            commandTexts.Add(currentCommandTexts.ToArray());
+                            currentCommandTexts.Clear();
+                        }
+                    }
+
+                    currentCommandTexts.Add(text);
+                }
+                else
+                {
+                    nonCommandTexts.Add(text);
+                }
+            }
+
+            if (currentCommandTexts.Count != 0)
+            {
+                commandTexts.Add(currentCommandTexts.ToArray());
+                currentCommandTexts.Clear();
+            }
+
+            var dataWriter = new DataWriter();
+
+            dataWriter.Write((byte)nonCommandTexts.Count);
+            foreach (var nonCommandText in nonCommandTexts)
+                dataWriter.WriteNullTerminated(nonCommandText, Encoding.UTF8);
+
+            dataWriter.Write((byte)commandTexts.Count);
+            foreach (var commandText in commandTexts)
+            {
+                dataWriter.Write((byte)commandText.Length);
+                foreach (var commandTextEntry in commandText)
+                    dataWriter.WriteNullTerminated(commandTextEntry, Encoding.UTF8);
+            }
+
+            File.WriteAllBytes(@"D:\Projekte\Ambermoon\Disks\Bugfixing\Polish\Intro_texts.amb", dataWriter.ToArray());
+        }
+    }
+}
